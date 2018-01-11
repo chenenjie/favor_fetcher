5a964c50efed2a2feff7cae3076764b4534dd3da
chenej <chenej@gzyitop.com>
2018-01-11 14:30:35 +0800

    finish single page  to do  total list

--- src/bin/fetch.rs
+++ src/bin/fetch.rs
@@ -4,6 +4,7 @@ extern crate error_chain;
 extern crate file_fetcher;
 extern crate regex;
 extern crate reqwest;
+extern crate scoped_threadpool;
 extern crate victoria_dom;
 
 use std::fs::{create_dir_all, File};
@@ -15,7 +16,9 @@ use reqwest::header::Headers;
 use reqwest::{Method, Url};
 use encoding::all::GBK;
 use encoding::{DecoderTrap, Encoding};
+use std::sync::mpsc::channel;
 use regex::Regex;
+use std::str::FromStr;
 
 mod errors {
     error_chain!{
@@ -25,19 +28,12 @@ mod errors {
         foreign_links{
             ReqwestError(::reqwest::Error);
             ParseIntError(::std::num::ParseIntError);
+            FileFetcherError(::file_fetcher::errors::Error);
         }
     }
 }
 
 fn main() {
-    // let result = file_fetcher::open_bytes_str("https://crates.io/assets/Cargo-Logo-Small-c39abeb466d747f3be442698662c5260.png").unwrap();
-    // let mut save_path = env::current_dir().unwrap();
-    // save_path.push("fucking_good.png");
-
-    // let mut file = File::create(save_path).unwrap();
-    // file.write_all(&*result);
-
-    // println!("{:?}", dir);
     println!("{:?}", fetcher());
 }
 
@@ -71,12 +67,11 @@ fn fetcher() -> errors::ComicResult<()> {
         Ok((pagnum.parse()?, title))
     })?;
 
-    println!("page {}, title {}", page, title);
 
     //创建存放的文件夹
-    let book_dir = creater_dir(&title);
+    let mut book_dir = creater_dir(&title);
 
-    let mut fetch_url_list: Vec<(i32, Url)> = {
+    let fetch_url_list: Vec<(i32, Url)> = {
         (2..(page + 1))
             .map(|i| {
                 let mut single_page = site_path.clone();
@@ -90,22 +85,47 @@ fn fetcher() -> errors::ComicResult<()> {
             .collect()
     };
 
-    let (succ_vec, fail_vec) : (Vec<(i32, Url, errors::ComicResult<String>)>, Vec<(i32, Url, errors::ComicResult<String>)>) = fetch_url_list.into_iter().map(|(num, single_page)|{
-        let url_entity = send_request(single_page.clone()).and_then(|result| {
 
-            let cap = re1.captures(&result).unwrap();
+    let (succ_vec, fail_vec): (
+        Vec<(i32, Url, errors::ComicResult<String>)>,
+        Vec<(i32, Url, errors::ComicResult<String>)>,
+    ) = fetch_url_list
+        .into_iter()
+        .map(|(num, single_page)| {
+            let url_entity = send_request(single_page.clone()).and_then(|result| {
+                let cap = re1.captures(&result).unwrap();
 
-            Ok(cap["img"].to_owned())
-        });
-        (num, single_page, url_entity)
-    }).partition(|&(_num, _single_page, url_entity)|{
-        url_entity.is_ok()
+                Ok(cap["img"].to_owned())
+            });
+            (num, single_page, url_entity)
+        })
+        .partition(|&(_num, ref _single_page, ref url_entity)| url_entity.is_ok());
+
+    println!("fail vec {:?}", fail_vec);
+
+    let length = succ_vec.len();
+
+    let mut pool = scoped_threadpool::Pool::new(4);
+
+    let (sender, receiver) = channel();
+
+    pool.scoped(|scope| {
+        for (_num, _single_page, url) in succ_vec {
+            let wrapper = sender.clone();
+            let book_dir_copy = book_dir.clone();
+            scope.execute(move || {
+                wrapper.send(download(
+                    Url::from_str(url.unwrap().as_str()).unwrap(),
+                    book_dir_copy,
+                ));
+            });
+        }
     });
 
-    println!("success vec {:?}", succ_vec);
-    println!("fail vec{:?}", fail_vec);
-
-    // println!("img_url {}", img_url.unwrap());
+    println!(
+        "success count {}",
+        receiver.iter().take(length).filter(|e| e.is_ok()).count()
+    );
 
     Ok(())
 }
@@ -120,14 +140,14 @@ fn creater_dir(dirname: &str) -> PathBuf {
     save_dir
 }
 
-fn download(url: Url, book_dir: PathBuf) {
-    let page_name = url.path_segments().unwrap();
-    let result = file_fetcher::open_bytes(url).unwrap();
-    let mut save_path = env::current_dir().unwrap();
-    save_path.push("fucking_good.png");
+fn download(url: Url, mut book_dir: PathBuf) -> errors::ComicResult<()> {
+    let page_name = url.path_segments().unwrap().last().unwrap().to_owned();
+    let result = file_fetcher::open_bytes(url)?;
+    book_dir.push(format!("{}", page_name));
 
-    let mut file = File::create(save_path).unwrap();
+    let mut file = File::create(book_dir).unwrap();
     file.write_all(&*result);
+    Ok(())
 }
 
 fn send_request(url: Url) -> errors::ComicResult<String> {
@@ -137,10 +157,10 @@ fn send_request(url: Url) -> errors::ComicResult<String> {
     let client = reqwest::Client::new();
     let mut resp = client.request(Method::Get, url).headers(headers).send()?;
 
-    println!("status {} ", resp.status().is_success());
+    // println!("status {} ", resp.status().is_success());
 
     let mut buf: Vec<u8> = vec![];
-    resp.copy_to(&mut buf).unwrap();
+    resp.copy_to(&mut buf)?;
 
     let body = GBK.decode(&*buf, DecoderTrap::Replace).unwrap();
 
